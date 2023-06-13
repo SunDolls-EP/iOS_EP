cdef2d94f30c744ec69b72959bab6c5977fd7365
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-06-13 23:24:59 +0900

    ✨ :: ProfileView, Reactor 생성

---
+++ iOS_EP/EaglePop/EaglePop/Sources/View/Tabbar/Profile/ProfileReactor.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileReactor.swift
+//  EaglePop
+//
+//  Created by 최시훈 on 2023/06/13.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import ReactorKit
+
+//class ProfileReactor: Reactor {
+//    
+//    
+//    
+//}
---
+++ iOS_EP/EaglePop/EaglePop/Sources/View/Tabbar/Profile/ProfileView.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileView.swift
+//  EaglePop
+//
+//  Created by 최시훈 on 2023/06/13.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import Then
+import SnapKit
+
+class ProfileView: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    
+}
