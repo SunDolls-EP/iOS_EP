c2a1dcfce54cafa15c7ed8855c0b8a9ea5eb6086
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-06-13 23:26:51 +0900

    ✨ :: RankingView, Reactor 생성

---
+++ iOS_EP/EaglePop/EaglePop/Sources/View/Tabbar/Ranking/RankingReactor.swift
@@ -0,0 +1,18 @@
+//
+//  RankingReactor.swift
+//  EaglePop
+//
+//  Created by 최시훈 on 2023/06/13.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import ReactorKit
+
+//class RankingReactor: Reactor {
+//    
+//    
+//    
+//    
+//}
---
+++ iOS_EP/EaglePop/EaglePop/Sources/View/Tabbar/Ranking/RankingView.swift
@@ -0,0 +1,23 @@
+//
+//  RankingView.swift
+//  EaglePop
+//
+//  Created by 최시훈 on 2023/06/13.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import Then
+import SnapKit
+
+class RankingView: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    
+}
