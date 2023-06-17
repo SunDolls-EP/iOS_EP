334f35962e38b7a5b2435cbd255167182787fd97
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-06-17 23:53:25 +0900

    ✨ :: stopWatchView 생성

--- iOS_EP/EaglePop/EaglePop/Sources/View/Tabbar/StopWatch/StopWatchView.swift
+++ iOS_EP/EaglePop/EaglePop/Sources/View/Tabbar/StopWatch/StopWatchView.swift
@@ -12,12 +12,29 @@ import Then
 import SnapKit
 
 class StopWatchView: UIViewController {
-    
+    let stopWatchView = UIView().then {
+        $0.backgroundColor = .red
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .mainBackgroundColor
+        setup()
         
     }
+}
+extension StopWatchView {
     
-    
+    func setup() {
+        [
+            stopWatchView
+        ].forEach { self.view.addSubview($0) }
+        stopWatchView.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(150)
+            $0.left.equalToSuperview().offset(61)
+            $0.right.equalToSuperview().offset(-61)
+            $0.bottom.equalTo(stopWatchView.snp.top).offset(270)
+        }
+        
+    }
 }
