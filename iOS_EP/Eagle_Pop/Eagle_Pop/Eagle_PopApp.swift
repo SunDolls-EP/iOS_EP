921f0d62d02fac9740cb3f5486649192fec1f8fe
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-08-23 16:50:15 +0900

    🎉 :: restart

---
+++ iOS_EP/Eagle_Pop/Eagle_Pop/Eagle_PopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Eagle_PopApp.swift
+//  Eagle_Pop
+//
+//  Created by 최시훈 on 2023/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Eagle_PopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
