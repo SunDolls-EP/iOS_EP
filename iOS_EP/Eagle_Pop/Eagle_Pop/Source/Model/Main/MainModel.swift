7ba3ac89d74a56b49e37b1d9c23b94e500d07082
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-09-09 23:51:01 +0900

    ✨ :: MainModel Implementation

---
+++ iOS_EP/Eagle_Pop/Eagle_Pop/Source/Model/Main/MainModel.swift
@@ -0,0 +1,24 @@
+//
+//  MainModel.swift
+//  Eagle_Pop
+//
+//  Created by 최시훈 on 2023/09/08.
+//
+
+import Foundation
+
+protocol MainModelStateProtocol {
+    
+}
+
+protocol MainModelActionsProtocol: AnyObject {
+    
+}
+
+final class MainModel: ObservableObject, MainModelStateProtocol {
+      
+}
+
+extension MainModel: MainModelActionsProtocol {
+    
+}
