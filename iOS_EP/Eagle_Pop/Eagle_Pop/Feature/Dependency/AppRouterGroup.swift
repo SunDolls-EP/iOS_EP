65d51b14f1f8c9504d23177377b188fa9a32426f
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-09-28 23:03:55 +0900

    ✨ :: LinkNavigator Dependency 추가

---
+++ iOS_EP/Eagle_Pop/Eagle_Pop/Feature/Dependency/AppRouterGroup.swift
@@ -0,0 +1,16 @@
+//
+//  AppRouterGroup.swift
+//  Eagle_Pop
+//
+//  Created by 최시훈 on 2023/09/26.
+//
+
+import LinkNavigator
+
+struct AppRouterGroup {
+  var routers: [RouteBuilder] {
+    [
+      MainRouteBuilder()
+    ]
+  }
+}
---
+++ iOS_EP/Eagle_Pop/Eagle_Pop/Feature/Navigator/AppDependency.swift
@@ -0,0 +1,10 @@
+//
+//  AppDependency.swift
+//  Eagle_Pop
+//
+//  Created by 최시훈 on 2023/09/26.
+//
+
+import LinkNavigator
+
+struct AppDependency: DependencyType { }
