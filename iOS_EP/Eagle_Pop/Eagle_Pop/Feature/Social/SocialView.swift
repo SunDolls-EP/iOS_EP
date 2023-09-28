dd17347f2fbfac9ec40c5fbe342bd36d5f0d72a6
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-09-28 23:01:27 +0900

    ✨ :: SocialView 초기커밋

---
+++ iOS_EP/Eagle_Pop/Eagle_Pop/Feature/Social/SocialView.swift
@@ -0,0 +1,22 @@
+//
+//  SocialView.swift
+//  Eagle_Pop
+//
+//  Created by 최시훈 on 2023/08/23.
+//
+
+import SwiftUI
+
+struct SocialView: View {
+    var body: some View {
+        
+        VStack {
+            
+        }
+        
+    }
+}
+
+#Preview {
+    SocialView()
+}
