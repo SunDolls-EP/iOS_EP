995d9396dd37b5ce47995e09e5329e573985dc43
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-10-04 22:51:49 +0900

    ✨ :: CircleProgressBar 줄어드는 부분에 원 추가

--- iOS_EP/Eagle_Pop/Eagle_Pop/Feature/Main/SubView/CircleProgressBar.swift
+++ iOS_EP/Eagle_Pop/Eagle_Pop/Feature/Main/SubView/CircleProgressBar.swift
@@ -10,8 +10,9 @@ import SwiftUI
 struct CircleProgressView: View {
     
     let percentage: CGFloat
+    let size: CGFloat = 16
+    
     var body: some View {
-
         ZStack {
             Circle()
                 .stroke(lineWidth: 3)
@@ -22,6 +23,13 @@ struct CircleProgressView: View {
                 .stroke(style: StrokeStyle(lineWidth: 8, lineCap: .round, lineJoin: .round))
                 .foregroundColor(Color.blue)
                 .rotationEffect(Angle(degrees: -90))
+            Circle()
+                .fill(Color.white)
+                .frame(width: size, height: size)
+                .overlay(Circle().strokeBorder(Color.accentColor, lineWidth: 2))
+                .padding(.top, -(size/2))
+                .frame(maxHeight: .infinity, alignment: .top)
+                .rotationEffect(Angle(degrees: percentage * 360))
         }
     }
 }
