e86cc145f84025c803cdfd6ed4c9b6be466af7e4
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-10-07 23:02:33 +0900

    ✨ :: MainView완성

--- iOS_EP/Eagle_Pop/Eagle_Pop/Feature/Main/MainCore.swift
+++ iOS_EP/Eagle_Pop/Eagle_Pop/Feature/Main/MainCore.swift
@@ -7,33 +7,45 @@
 
 import Foundation
 import ComposableArchitecture
+import AVFAudio
 
 struct MainCore: Reducer {
+    
+    var timer = Timer.publish(every: 1, on: .current, in: .common).autoconnect()
+    
     struct MainState: Equatable {
+        var paths: [String] = []
+        var message = ""
         var menuTitle: String = "주간 랭킹"
+        
     }
-
+    
     enum MainAction: Equatable {
         case selectDaily
         case selectWeekly
         case selectMonthly
+        // MARK: - TimerView
+        case soundPlay
     }
-
+    
     func reduce(into state: inout MainState, action: MainAction) -> Effect<MainAction> {
         switch action {
+            
         case .selectDaily:
             state.menuTitle = "일간 랭킹"
             return .none
-
+            
             
         case .selectWeekly:
             state.menuTitle = "주간 랭킹"
             return .none
-
+            
         case .selectMonthly:
             state.menuTitle = "월간 랭킹"
             return .none
+        case .soundPlay:
+            
+            return .none
         }
     }
-    
 }
--- iOS_EP/Eagle_Pop/Eagle_Pop/Feature/Main/MainView.swift
+++ iOS_EP/Eagle_Pop/Eagle_Pop/Feature/Main/MainView.swift
@@ -30,58 +30,73 @@ struct MainView: View {
                                 .cornerRadius(20)
                             
                             
-                            VStack {
-                                Menu("\(viewStore.menuTitle)") {
-                                    Button("일간 랭킹") { viewStore.send(.selectDaily) }
-                                    Button("주간 랭킹") { viewStore.send(.selectWeekly) }
-                                    Button("월간 랭킹") { viewStore.send(.selectMonthly) }
-                                }
-                                .padding(.top, 5)
-                                .font(.system(size: 25, weight: .semibold))
-                                HStack {
+//                            VStack {
+                                
+                                VStack {
+                                    HStack {
+                                        Menu("\(viewStore.menuTitle)") {
+                                            Button("일간 랭킹") { viewStore.send(.selectDaily) }
+                                            Button("주간 랭킹") { viewStore.send(.selectWeekly) }
+                                            Button("월간 랭킹") { viewStore.send(.selectMonthly) }
+                                        }
+//                                        .padding(.top, )
+                                        .font(.system(size: 25, weight: .semibold))
+                                        .foregroundColor(.black)
+                                        .padding(.leading, 10)
+                                        
+                                        Spacer()
+                                    }
+                                    
                                     Button(action: {
                                         // 추후 RankingView로 이동하는 코드 작성
                                     }) {
                                         let sizes = [90, 110, 85]
                                         let names = ["황주완", "최시훈", "이혜성"]
+                                        let images = ["Juwan", "Sihun", "Hyeseong"]
                                         ForEach(0..<sizes.count, id: \.self) { index in
                                             let size = sizes[index]
                                             let name = names[index]
-                                            
+                                            let image = images[index]
                                             VStack {
-                                                Image(systemName: "person.fill")
+                                                Image(image)
                                                     .resizable()
-                                                    .foregroundColor(.gray)
-                                                    .background(Color.black)
+                                                    .foregroundColor(Color("default"))
+                                                    .background(Color.gray)
                                                     .frame(width: CGFloat(size), height: CGFloat(size))
                                                     .cornerRadius(CGFloat(size) / 2)
-                                                    .font(.headline)
+                                                    .font(.title)
                                                     .frame(width: size != 110 ? 90 : nil)
                                                 
                                                 Text(name)
                                                     .font(.custom("pretendardMedium", size: size == 110 ? 30 : 25))
                                                     .foregroundColor(.black)
                                             }
-                                            
                                         }
-                                        
                                     }
                                     .onAppear() {
                                         
                                     }
                                 }
-                                .padding(.vertical, 25)
+                                .padding(.vertical, 20)
                                 .padding(.horizontal, 20)
                                 .background(.white)
                                 .cornerRadius(20)
-                            }
-                            .padding(20)
+                                .padding(.horizontal, 20)
+                                .padding(.top, 30)
+
+//                            }
+//                            .padding(20)
                         }
                         .padding(.top, 10)
                         .frame(maxWidth: .infinity)
                     }
                     .navigationBarTitle(Text("Eagle Pop"), displayMode: .inline)
                     .background(TossColor.background.ignoresSafeArea())
+                    .refreshable{
+                        func getSomeData() async {
+                            await Task.sleep(3_000_000_000)
+                        }
+                    }
             }
         }
     }
