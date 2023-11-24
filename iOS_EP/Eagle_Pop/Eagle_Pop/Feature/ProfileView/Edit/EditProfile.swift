52c23d8955c534caf26a4684ee3ad3db609221ef
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-11-24 23:09:18 +0900

    ✨ :: EditProfileModel 추가

---
+++ iOS_EP/Eagle_Pop/Eagle_Pop/Feature/ProfileView/Edit/EditProfile.swift
@@ -0,0 +1,13 @@
+//
+//  EditProfile.swift
+//  Eagle_Pop
+//
+//  Created by 최시훈 on 11/1/23.
+//
+
+import Foundation
+
+struct EditProfile: Codable {
+    var username: String
+    var schoolName: String
+}
