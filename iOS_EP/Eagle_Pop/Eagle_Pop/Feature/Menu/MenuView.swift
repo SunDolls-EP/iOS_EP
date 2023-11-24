d63efdebdb72b33c8ecc09c8dda2b2919857bbf1
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-11-24 23:14:03 +0900

    ✨ :: MenuViewUI 구현

--- iOS_EP/Eagle_Pop/Eagle_Pop/Feature/Menu/MenuView.swift
+++ iOS_EP/Eagle_Pop/Eagle_Pop/Feature/Menu/MenuView.swift
@@ -11,26 +11,42 @@ import OpenTDS
 
 struct MenuView: View {
     let store: StoreOf<MenuCore>
-        
+    
+    
     var body: some View {
         WithViewStore(self.store, observe: { $0 }) { viewStore in
             NavigationView {
-                    ScrollView {
+                List {
+//                    Section {
                         ProfileView(store: Store(initialState:  MenuCore.State()) {
                             MenuCore()
                         })
                         .padding(.top, -10)
+                        .padding(.bottom, 20)
+//                    }
+//                    .onTapGesture{
+//                        viewStore.send(.editProfile)
+//                    }
+                    
+                    AppInfoView(store: Store(initialState:  AppInfoCore.State()) {
+                        AppInfoCore()
+                    })
+                }
+                .navigationBarTitle(Text("프로필"), displayMode: .large)
 
-                        Spacer()
-                    }
-                    .navigationBarTitle(Text("Hello, SwiftUI!"), displayMode: .inline)
-                    .background(TossColor.background.ignoresSafeArea())
+                
             }
+            .padding(.horizontal, 0)
+            .background(TossColor.background.ignoresSafeArea())
+            .listStyle(InsetGroupedListStyle())
+            
         }
+        .padding(.horizontal, 0)
     }
 }
 
+
 #Preview {
-        MenuView(store: Store(initialState:  MenuCore.State()) {
-            MenuCore()})
+    MenuView(store: Store(initialState:  MenuCore.State()) {
+        MenuCore()})
 }
