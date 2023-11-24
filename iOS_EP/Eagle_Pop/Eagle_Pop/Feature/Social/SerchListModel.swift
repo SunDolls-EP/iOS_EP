49e6b7ec81d3ca34e12f0950b2a5dd8a95a2a71c
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-11-24 23:22:27 +0900

    ✨ :: SerchListModel 추가

--- iOS_EP/Eagle_Pop/Eagle_Pop/Feature/Social/SerchListModel.swift
+++ iOS_EP/Eagle_Pop/Eagle_Pop/Feature/Social/SerchListModel.swift
@@ -1,21 +1,18 @@
 //
-//  SerchListModel.swift
+//  RandomList.swift
 //  Eagle_Pop
 //
-//  Created by 최시훈 on 10/5/23.
+//  Created by 최시훈 on 2023/11/07.
 //
 
 import Foundation
  
-struct SerchListInfo: Hashable, Identifiable {
-    let id = UUID()
-    let rank: Int
-    let image: String
-    let name: String
-    let groupName: String
-}
-
-struct SerchListArray: Hashable,Identifiable {
-    let id = UUID()
-    let serchListInfo: [SerchListInfo]
+struct RandomList: Hashable, Decodable {
+    let username: String?
+    let tag: String?
+    let schoolName: String?
+    let totalStudyTime: Int?
+    let profileUrl: String?
+    let createdAt: String?
+    let modifiedAt: String?
 }
