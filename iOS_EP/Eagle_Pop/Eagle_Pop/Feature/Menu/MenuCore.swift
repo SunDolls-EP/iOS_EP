66f34cfd7559157c4bd33e4b7a29031c3a7d0558
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-11-26 10:48:15 +0900

    ✨ :: ICT 출품 대비 UI 업데이트

--- iOS_EP/Eagle_Pop/Eagle_Pop/Feature/Menu/MenuCore.swift
+++ iOS_EP/Eagle_Pop/Eagle_Pop/Feature/Menu/MenuCore.swift
@@ -12,7 +12,7 @@ struct MenuCore: Reducer {
     struct MenuState: Equatable {
         var memberName: String = "최시훈"
         var menuTitle: String = "이번 주 공부한 시간"
-        var studyTime: String = "01:53"
+        var studyTime: String = "13,833"
         var affiliated: String = "대구소프트웨어마이스터고등학교"
     }
     
@@ -20,16 +20,17 @@ struct MenuCore: Reducer {
         case getMemberInfo
         case getAffiliated
         case editProfile
+        case totalStudyTime
         case selectDailyStudyTime
         case selectWeeklyStudyTime
         case selectMonthlyStudyTime
         case saveModifiedInfo
-        case selectAppInfo(title: String)
     }
     
-//    @Dependency(\.sideEffect.menu) var sideEffect
-
+    @Dependency(\.sideEffect.menu) var sideEffect
+    
     func reduce(into state: inout MenuState, action: MenuAction) -> Effect<MenuAction> {
+        var state = state
         switch action {
         case .getMemberInfo:
             
@@ -39,36 +40,68 @@ struct MenuCore: Reducer {
             return .none
             
         case .editProfile:
-            
+            sideEffect.routeToEditProfile()
+            print("1234")
             return .none
-            
+        case .totalStudyTime:
+            state.menuTitle = "총 공부한 시간"
+//            Requests.simple("\(api)/user/study",
+//                .get,
+//                params: ["Authorization": "\(Token.get(.accessToken)!)"]) { responseData in
+//                let decoder = JSONDecoder()
+//                if let data = responseData, let decodedData = try? decoder.decode(StudyTime.self, from: data) {
+                    state.studyTime = "0ㄷ"
+//                } else {
+//                    print("Error decoding data.")
+//                }
+//            }
+
+            return .none
         case .selectDailyStudyTime:
             state.menuTitle = "오늘 공부한 시간"
+//            Requests.simple("\(api)/user/study",
+//                .get,
+//                params: ["Authorization": "\(Token.get(.accessToken)!)"]) { responseData in
+//                let decoder = JSONDecoder()
+//                if let data = responseData, let decodedData = try? decoder.decode(StudyTime.self, from: data) {
+//                    state.studyTime = "\(decodedData.totalTime)"
+//                } else {
+//                    print("Error decoding data.")
+//                }
+//            }
+
             return .none
             
         case .selectWeeklyStudyTime:
             state.menuTitle = "이번 주 공부한 시간"
+//            Requests.simple("\(api)/user/study", 
+//                .get,
+//                params: ["Authorization": "\(Token.get(.accessToken)!)"]) { responseData in
+//                let decoder = JSONDecoder()
+//                if let data = responseData, let decodedData = try? decoder.decode(StudyTime.self, from: data) {
+//                    state.studyTime = "\(decodedData.totalTime)"
+//                } else {
+//                    print("Error decoding data.")
+//                }
+//            }
             return .none
             
         case .selectMonthlyStudyTime:
             state.menuTitle = "이번 달 공부한 시간"
+            Requests.simple("\(api)/user/study",
+                .get,
+                params: ["Authorization": "\(Token.get(.accessToken)!)"]) { responseData in
+                let decoder = JSONDecoder()
+                if let data = responseData, let decodedData = try? decoder.decode(StudyTime.self, from: data) {
+                    state.studyTime = "\(decodedData.totalTime)"
+                } else {
+                    print("예기치 못한 오류")
+                }
+            }
+
             return .none
         case .saveModifiedInfo:
             
-            return .none
-        case .selectAppInfo(let title):
-            switch title {
-            case "오픈소스 라이선스":
-                print("오픈소스 라이선스 버튼을 눌렀습니다.")
-            case "개발자 정보":
-                print("개발자 정보 버튼을 눌렀습니다.")
-            case "버전 정보":
-                print("버전 정보 버튼을 눌렀습니다.")
-            case "로그아웃":
-                print("로그아웃 버튼을 눌렀습니다.")
-            default:
-                break
-            }
             return .none
         }
     }
